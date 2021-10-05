fe4de61e31f6283f54aebe9e5912fb448c0a92b6
Milton-Neves <milzonzneves@gmail.com>
2021-10-05 09:36:47 -0300

    criar editar questao quatro

--- lista04/mail.py
+++ lista04/mail.py
@@ -6,17 +6,17 @@ from time import sleep
 
 def send_message(email, name, html):
     with SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(email, password)
+        server.login(email, 'Lalala1.2')
 
         msg = MIMEMultipart()
-        msg['Subject'] = SUBJECT
-        msg['From'] = email_de
-        msg['To'] = email_para
+        msg['Subject'] = 'E-mail enviado com sucesso'
+        msg['From'] = 'milzonzneves@gmail.com'
+        msg['To'] = 'questaoquatro@gmail.com'
         html = MIMEText(html, 'html')
         msg.attach(html)
 
         text = msg.as_string()
 
-        server.send_message(msg, text)
+        server.send_message(msg ['From'], msg ['To'], text ['Subject'])
         print(f"Email enviado para: {name}")
         sleep(2)
